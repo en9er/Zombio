f3b73fb47433d22ef66cc6f2a8f5f53d366be4b7
en9er <frendjoy55@gmail.com>
2023-06-27 02:35:35 +0500

    Added player movement and animations

---
+++ Assets/Camera/Scripts/MouseLook.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float mouseSencitivity = 100f;
+    public Transform playerBody;
+    float xRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSencitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSencitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -70f, 70f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Player/Scripts/PlayerController.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public Animator animator;
+
+    Vector3 velocity;
+    bool isGrounded;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0 )
+        {
+            velocity.y = -2f;
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            isGrounded = false;
+
+        }
+
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        
+        if (z > 0)
+        {
+            animator.SetBool("runningBackward", false);
+            animator.SetBool("runningForward", true);
+        }
+        else if(z == 0)
+        {
+            animator.SetBool("runningBackward", false);
+            animator.SetBool("runningForward", false);
+        }
+        else
+        {
+            animator.SetBool("runningBackward", true);
+            animator.SetBool("runningForward", false);
+        }
+
+        if (x > 0) 
+        {
+            animator.SetBool("runningLeft", false);
+            animator.SetBool("runningRight", true);
+        }
+        else if(x == 0)
+        {
+            animator.SetBool("runningLeft", false);
+            animator.SetBool("runningRight", false);
+        }
+        else
+        {
+            animator.SetBool("runningLeft", true);
+            animator.SetBool("runningRight", false);
+        }
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
